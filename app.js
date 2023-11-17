fd94d263dc06958dbeb2059fc5d28103f542faf1
carloswelliton7@gmail.com <carloswelliton7@gmail.com>
2023-11-16 23:20:22 -0400

    update curso de node

--- app.js
+++ app.js
@@ -6,34 +6,62 @@
     const admin = require ('./routes/admin');
     const path = require('path');
     const mongoose = require ('mongoose');
+    const session = require('express-session');
+    const flash = require('connect-flash');
 
 // configurações
+    //Sessão
+        app.use(session(
+        {
+            secret: "cursodenode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    //Middleware
+        app.use((req, res, next) => 
+        {
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next()
+        })
     //Body-parser foi descontinuado, deve-se usar o express
-    app.use(express.urlencoded({extended: true}));
-    app.use(express.json());
+        app.use(express.urlencoded({extended: true}));
+        app.use(express.json());
+
     //Handlebars
-    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
-    app.set('view engine', 'handlebars');
+        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
+
     //Mongoose
         mongoose.Promise = global.Promise
-        mongoose.connect("mongodb://127.0.0.1:27017/blogapp", {useNewUrlParser: true}).then(() => {
+        mongoose.connect("mongodb://127.0.0.1:27017/blogapp", {useNewUrlParser: true}).then(() => 
+        {
             console.log("Conectado ao Mongo")
-        }).catch((err) => {
+        }).catch((err) => 
+        {
             console.log("Erro ao se conectar", err)
         })
+
     //Public
         app.use(express.static(path.join(__dirname,'public')));
+        //app.use((req, res, next) => {
+        //    console.log("Oi eu sou um Middleware")
+        //    next()
+        //})
 
 //rotas (sempre chamar embaixo das configurações)
-    app.get('/', (req, res) => {
+    app.get('/', (req, res) => 
+    {
         res.send("Pagina principal!")
     })
     app.use('/admin', admin);
 
 
 //outros
-const PORT = 8081;
-app.listen(PORT, () => {
-    console.log('Servidor rodando na URL http://localhost:8081');
-});
+    const PORT = 8081;
+    app.listen(PORT, () => 
+    {
+        console.log('Servidor rodando na URL http://localhost:8081');
+    });
 
--- routes/admin.js
+++ routes/admin.js
@@ -4,32 +4,60 @@ const mongoose = require("mongoose");
 require("../models/Categoria");
 const Categoria = mongoose.model("categorias");
 
-router.get('/', (req,res) => {
+router.get('/', (req,res) => 
+{
     res.render("admin/index")
 });
 
-router.get('/posts', (req, res) => {
+router.get('/posts', (req, res) => 
+{
     res.render('admin/posts')
 });
 
-router.get('/categorias', (req,res) => {
+router.get('/categorias', (req,res) => 
+{
     res.render("admin/categorias")
 });
 
-router.get('/categorias/add', (req,res) =>{
+router.get('/categorias/add', (req,res) =>
+{
     res.render('admin/addcategoria')
 })
-router.post("/categorias/nova", (req, res) => {
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
-    }
+router.post("/categorias/nova", (req, res) => 
+{
 
-    new Categoria(novaCategoria).save().then(() => {
-        console.log('Categoria salva com sucesso')
-    }).catch((err) => {
-        console.log('Erro ao salvar categoria: ', err)
-    });
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null)
+    {
+        erros.push({texto: "Nome inválido"})
+    }
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null)
+    {
+        erros.push({texto: "Slug inválido"})
+    }
+    if(req.body.slug.length < 2)
+    {
+        erros.push({texto: "Nome da categoria muito pequeno"})
+    }
+    if(erros.length > 0)
+    {
+        res.render("admin/addcategoria", {erros: erros})
+    }else{
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+    
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash("success_msg", "Categoria criada com sucesso")
+            res.redirect('/admin/categorias')
+        }).catch((err) => 
+        {
+            req.flash("error_msg", "Houve um erro ao salvar categoria, tente novamente")
+            res.redirect("/admin")
+        });
+    }
 })
 
 
