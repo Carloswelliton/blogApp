21c32b20b25e7cf9a231da27b080606e4efd6369
carloswelliton7@gmail.com <carloswelliton7@gmail.com>
2024-01-13 16:43:49 -0400

    listando postagens em paginas unicas e iniciando a listagem de categorias, configurando suas rotas

--- app.js
+++ app.js
@@ -10,6 +10,8 @@
     const flash = require('connect-flash');
     require("./models/Postagem")
     const Postagem = mongoose.model("postagens")
+    require('./models/Categoria')
+    const Categoria = mongoose.model('categorias')
 
 // configurações
     //Sessão
@@ -64,10 +66,44 @@
     })
 
     app.get('/postagem/:slug', (req, res) => {
-        Postagem.findOne({slug: req.params.slug}).then((postagem) => {
+        Postagem.findOne({slug: req.params.slug}).lean().then((postagem) => {
             if(postagem){
-
+                res.render('postagem/index', {postagem: postagem})
+            }else{
+                req.flash('error_msg', 'Essa postagem não exite!')
+                res.redirect('/')
             }
+        }).catch((err) => {
+            req.flasj('error_msg', 'Houve um erro interno')
+            res.redirect('/')
+        })
+    })
+
+    app.get('/categorias', (req, res) => {
+        Categoria.find().lean().then((categorias) => {
+            res.render('categorias/index', {categorias: categorias})
+        }).catch((erro) => {
+            req.flash('error_msg', 'Houve um erro interno ao listar as categorias')
+            res.redirect('/')
+        })
+    })
+
+    app.get('/cateorias/:slug', (req, res) => {
+        Categoria.findOne({slug: req.params.slug}).lean().then((categoria) => {
+            if(categoria){
+                Postagem.find({categoria: categoria._id}).then((postagens) => {
+                    res.render('categorias/postagens', {postagens: postagens, categoria: categoria})
+                }).catch((err) => {
+                    req.flash('error_msg', "Houve um erro ao listar os posts!")
+                    res.redirect('/')
+                })
+            }else{
+                req.flash('error_msg', "Esta categoria não existe")
+                res.redirect('/')
+            }
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro interno ao carregar a página desta categoria')
+            res.redirect('/')
         })
     })
 
